c60beeb1c7f849d422dab09dd592c79bd20d3b37
Jack Wesley Riley <wes.riley@guidepointsecurity.com>
2021-03-20 11:06:21 -0500

    Added button to discover page

--- src/plugins/discover/public/application/angular/doc_table/components/table_row.ts
+++ src/plugins/discover/public/application/angular/doc_table/components/table_row.ts
@@ -32,6 +32,13 @@ import cellTemplateHtml from '../components/table_row/cell.html';
 import truncateByHeightTemplateHtml from '../components/table_row/truncate_by_height.html';
 import { esFilters } from '../../../../../../data/public';
 import { getServices } from '../../../../kibana_services';
+<<<<<<< HEAD
+=======
+import { getContextUrl } from '../../../helpers/get_context_url';
+import { formatRow, formatTopLevelObject } from '../../helpers';
+// GPS-DFIR Modification
+import {send} from '../../../../../../../plugins/console/public/lib/es/es'
+>>>>>>> 76b63d319be... Added button to discover page
 
 const TAGS_WITH_WS = />\s+</g;
 
@@ -140,7 +147,8 @@ export function createTableRowDirective($compile: ng.ICompileService) {
       function createSummaryRow(row: any) {
         const indexPattern = $scope.indexPattern;
         $scope.flattenedRow = indexPattern.flattenHit(row);
-
+        // GPS-DFIR Modification
+        indexPattern.irstatus = 'gpsdfir_status';
         // We just create a string here because its faster.
         const newHtmls = [openRowHtml];
 
@@ -150,6 +158,8 @@ export function createTableRowDirective($compile: ng.ICompileService) {
           newHtmls.push(
             cellTemplate({
               timefield: true,
+              // GPS-DFIR Modification
+              gpsdfir: false,
               formatted: _displayField(row, indexPattern.timeFieldName),
               filterable: mapping(indexPattern.timeFieldName).filterable && $scope.filter,
               column: indexPattern.timeFieldName,
@@ -157,8 +167,50 @@ export function createTableRowDirective($compile: ng.ICompileService) {
           );
         }
 
+<<<<<<< HEAD
         $scope.columns.forEach(function (column: any) {
           const isFilterable = mapping(column) && mapping(column).filterable && $scope.filter;
+=======
+        // GPS-DFIR Modification
+        if ($scope.flattenedRow['gpsdfir_status'] == 'malicious') {
+          $scope.irstatus = true;
+        } else if ($scope.flattenedRow['gpsdfir_status'] == 'unknown') {
+          $scope.irstatus = false;
+        }
+
+        $scope.toggleStatus = function toggleStatus() {
+          const method = "POST";
+          const path = $scope.row._index + "/_update_by_query";
+          const data =  "{\n" +
+                        "  \"script\": {\n" +
+                        "    \"inline\": \"if (ctx._source.gpsdfir_status == \\\"malicious\\\"){ctx._source.gpsdfir_status = \\\"unknown\\\"} else {ctx._source.gpsdfir_status = \\\"malicious\\\"}\"," +
+                        "    \"lang\": \"painless\"\n" +
+                        "  },\n" +
+                        "  \"query\": {\n" +
+                        "    \"match\": {\n" +
+                        "      \"_id\": \"" + $scope.row._id + "\"\n" +
+                        "    }\n" +
+                        "  }\n" +
+                        "}\n";
+          console.log("Query-on-click: " + method + " " + path + " " + data);
+
+          send(method,path,data);
+        }
+        //console.log(indexPattern.fields.byName[row,'gpsdfir_status']);
+        //console.log(_displayField(row, 'gpsdfir_status'));
+        //console.log($scope.flattenedRow['gpsdfir_status']);
+        newHtmls.push(cellTemplate({
+          gpsdfir: true,
+          timefield: false,
+          formatted: '<button class="kuiButton" ng-click="$parent.irstatus = !$parent.irstatus" ng-class="{  \'kuiButton--danger\': $parent.irstatus, \'kuiButton--basic\': !$parent.irstatus }"><span class="kuiButton__icon kuiIcon fa-plus"></span></button>',
+          filterable: false,
+          column: 'gpsdfir_status'
+        }));
+        // GPS-DFIR Modification End
+
+        if ($scope.columns.length === 0 && $scope.useNewFieldsApi) {
+          const formatted = formatRow(row, indexPattern);
+>>>>>>> 76b63d319be... Added button to discover page
 
           newHtmls.push(
             cellTemplate({
@@ -169,7 +221,42 @@ export function createTableRowDirective($compile: ng.ICompileService) {
               column,
             })
           );
+<<<<<<< HEAD
         });
+=======
+        } else {
+          $scope.columns.forEach(function (column: string) {
+            const isFilterable = mapping(column) && mapping(column).filterable && $scope.filter;
+            if ($scope.useNewFieldsApi && !mapping(column) && !row.fields[column]) {
+              const innerColumns = Object.fromEntries(
+                Object.entries(row.fields).filter(([key]) => {
+                  return key.indexOf(`${column}.`) === 0;
+                })
+              );
+              newHtmls.push(
+                cellTemplate({
+                  timefield: false,
+                  sourcefield: true,
+                  formatted: formatTopLevelObject(row, innerColumns, indexPattern),
+                  filterable: false,
+                  column,
+                })
+              );
+            } else {
+              newHtmls.push(
+                cellTemplate({
+                  timefield: false,
+                  gpsdfir: false,
+                  sourcefield: column === '_source',
+                  formatted: _displayField(row, column, true),
+                  filterable: isFilterable,
+                  column,
+                })
+              );
+            }
+          });
+        }
+>>>>>>> 76b63d319be... Added button to discover page
 
         let $cells = $el.children();
         newHtmls.forEach(function (html, i) {
